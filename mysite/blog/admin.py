d3224385075f548b69969d785503d75a8a3f9fb4
smtsuchi <shoha1999@gmail.com>
2021-08-10 22:29:26 -0400

    tuesday changes

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+# import Post
+from .models import Post
+
 # Register your models here.
+admin.site.register(Post)
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='blog-index'),
     path('about/', views.aboutPage, name='blog-about'),
-    path('contact/', views.aboutPage, name='blog-contact'),
+    path('lol/', views.aboutPage, name='blog-about'),
+    path('posts/', views.posts, name='blog-posts'),
+    path('posts/create', views.createPost, name='blog-createpost'),
+    path('register/', views.registerPage, name='blog-register'),
+    path('login/', views.loginPage, name='blog-login'),
+    path('logout/', views.logoutUser, name='blog-logout'),
 ]
